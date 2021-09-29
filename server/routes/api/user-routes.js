6b7e0af88b9f646c4b4bc2f650614390faea32b8
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-29 12:15:25 -0700

    added mutations for addUser, loginUser, saveFood, and removeFood

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const {
   createUser,
   getSingleUser,
-  addFood,
+  saveFood,
   removeFood,
   login,
 } = require("../../controllers/user-controller");
@@ -11,7 +11,7 @@ const {
 const { authMiddleware } = require("../../utils/auth");
 
 // put authMiddleware anywhere we need to send a token for verification of the user
-router.route("/").post(createUser).put(authMiddleware, addFood);
+router.route("/").post(createUser).put(authMiddleware, saveFood);
 
 router.route("/login").post(login);
 
