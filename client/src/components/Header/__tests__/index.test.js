a6332017ebd97df1ac8eddd73c212c7fa7982402
Samantha Malone <sammac99@gmail.com>
2021-10-01 11:58:56 -0700

    added first header test

--- client/src/components/Header/__tests__/index.test.js
+++ client/src/components/Header/__tests__/index.test.js
@@ -1,16 +1,17 @@
-import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import Header from "..";
+import AppNavbar from "..";
 
-const mockToggleHeader = jest.fn();
-const currentRecipe = {
-  title: "Caldo de Queso",
-  link: "caldos.com",
-  source: "Mi Abuela",
-  totalTime: 2,
-  ingredients: ["papas, queso, caldo, pablanos"],
-};
+// const mockToggleHeader = jest.fn();
+// const currentRecipe = {
+//   title: "Caldo de Queso",
+//   link: "caldos.com",
+//   source: "Mi Abuela",
+//   totalTime: 2,
+//   ingredients: ["papas, queso, caldo, pablanos"],
+// };
 
 afterEach(cleanup);
 
@@ -18,6 +19,24 @@ describe("Header component", () => {
   //renders Header test
   // First Test
   it("renders", () => {
-    render(<Header onClose={mockToggleHeader} currentRecipe={currentRecipe} />);
+    render(<AppNavbar />);
   });
 });
+
+//   it("matches snapshot DOM node structure", () => {
+//     const { asFragment } = render(
+//       <Header onClose={mockToggleHeader} currentRecipe={currentRecipe} />
+//     );
+//     expect(asFragment()).toMatchSnapshot();
+//   });
+// });
+
+// describe("Click Event", () => {
+//   it("calls onClose handler", () => {
+//     const { getByText } = render(
+//       <Modal onClose={mockToggleHeader} currentRecipe={currentRecipe} />
+//     );
+//     fireEvent.click(getByText("Close this Header"));
+//     expect(mockToggleHeader).toHaveBeenCalledTimes(1);
+//   });
+// })
