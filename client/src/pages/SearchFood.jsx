1809b4b432643cfa0994a8ee8efb2dbceb2d22d5
Marco Avila <mavila6@email.arizona.com>
2021-09-30 19:32:12 -0700

    added useSate variables and imports to the searchfood page

--- client/src/pages/SearchFood.jsx
+++ client/src/pages/SearchFood.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
-import {
-  Jumbotron,
-  Container,
-  Col,
-  Form,
-  Button,
-  Card,
-  CardColumns,
-} from "react-bootstrap";
+// import {
+//   Jumbotron,
+//   Container,
+//   Col,
+//   Form,
+//   Button,
+//   Card,
+//   CardColumns,
+// } from "react-bootstrap"; This will be changed to material ui
 
 import { useMutation } from "@apollo/client";
 import { SAVE_FOOD } from "../utils/mutations";
 import Auth from "../utils/auth";
-import {searchResults}
+import {searchResult} from "../utils/API";
+import { saveFoodIds, getSavedFoodIds} from "../utils/localStorage";
+
+const SearchFood = () => {
+  const [searchedFood, setSearchedFood] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [savedFoodIds, setSavedFoodIds] = useState(getSavedFoodIds());
+  const [foodSave] = useMutation(SAVE_FOOD);
+
+
+}
