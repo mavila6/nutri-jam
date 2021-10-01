dcad0b365c608d098e1708f99bd94ab30efba511
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-30 22:32:19 -0700

    added change for who cares

--- client/src/pages/SavedFood.jsx
+++ client/src/pages/SavedFood.jsx
@@ -39,50 +39,50 @@ const SavedFood = () => {
     }
   };
 
-  return (
-    <>
-      <Jumbotron fluid className="text-light bg-dark">
-        <Container>
-          <h1>Viewing saved books!</h1>
-        </Container>
-      </Jumbotron>
-      <Container>
-        <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? "book" : "books"
-              }:`
-            : "You have no saved books!"}
-        </h2>
-        <CardColumns>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Card key={book.bookId} border="dark">
-                {book.image ? (
-                  <Card.Img
-                    src={book.image}
-                    alt={`The cover for ${book.title}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className="small">Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
-                  <Button
-                    className="btn-block btn-danger"
-                    onClick={() => handleDeleteBook(book.bookId)}
-                  >
-                    Delete this Book!
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardColumns>
-      </Container>
-    </>
-  );
+//   return (
+//     <>
+//       <Jumbotron fluid className="text-light bg-dark">
+//         <Container>
+//           <h1>Viewing saved books!</h1>
+//         </Container>
+//       </Jumbotron>
+//       <Container>
+//         <h2>
+//           {userData.savedBooks.length
+//             ? `Viewing ${userData.savedBooks.length} saved ${
+//                 userData.savedBooks.length === 1 ? "book" : "books"
+//               }:`
+//             : "You have no saved books!"}
+//         </h2>
+//         <CardColumns>
+//           {userData.savedBooks.map((book) => {
+//             return (
+//               <Card key={book.bookId} border="dark">
+//                 {book.image ? (
+//                   <Card.Img
+//                     src={book.image}
+//                     alt={`The cover for ${book.title}`}
+//                     variant="top"
+//                   />
+//                 ) : null}
+//                 <Card.Body>
+//                   <Card.Title>{book.title}</Card.Title>
+//                   <p className="small">Authors: {book.authors}</p>
+//                   <Card.Text>{book.description}</Card.Text>
+//                   <Button
+//                     className="btn-block btn-danger"
+//                     onClick={() => handleDeleteBook(book.bookId)}
+//                   >
+//                     Delete this Book!
+//                   </Button>
+//                 </Card.Body>
+//               </Card>
+//             );
+//           })}
+//         </CardColumns>
+//       </Container>
+//     </>
+//   );
 };
 
 export default SavedFood;
