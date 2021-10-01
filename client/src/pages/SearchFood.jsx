497545e88d266cf365a7fcbc4cebc9b80e1eb62d
Marco Avila <mavila6@email.arizona.com>
2021-09-30 21:18:41 -0700

    added placeholder return in the searchFood jsx file

--- client/src/pages/SearchFood.jsx
+++ client/src/pages/SearchFood.jsx
@@ -71,7 +71,77 @@ const SearchFood = () => {
     }
   };
 
-  
+  // return (
+  //   <>
+  //     <Jumbotron fluid className="text-light bg-dark">
+  //       <Container>
+  //         <h1>Search for Books!</h1>
+  //         <Form onSubmit={handleFormSubmit}>
+  //           <Form.Row>
+  //             <Col xs={12} md={8}>
+  //               <Form.Control
+  //                 name="searchInput"
+  //                 value={searchInput}
+  //                 onChange={(e) => setSearchInput(e.target.value)}
+  //                 type="text"
+  //                 size="lg"
+  //                 placeholder="Search for a book"
+  //               />
+  //             </Col>
+  //             <Col xs={12} md={4}>
+  //               <Button type="submit" variant="success" size="lg">
+  //                 Submit Search
+  //               </Button>
+  //             </Col>
+  //           </Form.Row>
+  //         </Form>
+  //       </Container>
+  //     </Jumbotron>
+
+  //     <Container>
+  //       <h2>
+  //         {searchedBooks.length
+  //           ? `Viewing ${searchedBooks.length} results:`
+  //           : "Search for a book to begin"}
+  //       </h2>
+  //       <CardColumns>
+  //         {searchedBooks.map((book) => {
+  //           return (
+  //             <Card key={book.bookId} border="dark">
+  //               {book.image ? (
+  //                 <Card.Img
+  //                   src={book.image}
+  //                   alt={`The cover for ${book.title}`}
+  //                   variant="top"
+  //                 />
+  //               ) : null}
+  //               <Card.Body>
+  //                 <Card.Title>{book.title}</Card.Title>
+  //                 <p className="small">Authors: {book.authors}</p>
+  //                 <Card.Text>{book.description}</Card.Text>
+  //                 {Auth.loggedIn() && (
+  //                   <Button
+  //                     disabled={savedBookIds?.some(
+  //                       (savedBookId) => savedBookId === book.bookId
+  //                     )}
+  //                     className="btn-block btn-info"
+  //                     onClick={() => handleSaveBook(book.bookId)}
+  //                   >
+  //                     {savedBookIds?.some(
+  //                       (savedBookId) => savedBookId === book.bookId
+  //                     )
+  //                       ? "This book has already been saved!"
+  //                       : "Save this Book!"}
+  //                   </Button>
+  //                 )}
+  //               </Card.Body>
+  //             </Card>
+  //           );
+  //         })}
+  //       </CardColumns>
+  //     </Container>
+  //   </>
+  // ); Will be replaced with our Material UI code
 };
 
 export default SearchFood;
