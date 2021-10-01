23085d5589e6e9c2850e543dd1b2e027fea05619
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-30 19:08:02 -0700

    added more files

--- client/src/pages/SearchFood.jsx
+++ client/src/pages/SearchFood.jsx
@@ -1,4 +1,15 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
-    Jumbotron, Con
-}
+  Jumbotron,
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+} from "react-bootstrap";
+
+import { useMutation } from "@apollo/client";
+import { SAVE_FOOD } from "../utils/mutations";
+import Auth from "../utils/auth";
+import {searchResults}
