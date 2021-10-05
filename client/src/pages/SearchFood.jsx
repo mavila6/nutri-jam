27c27a44f58d5657b976fb12d554c55d60344cbe
Samantha Malone <sammac99@gmail.com>
2021-10-05 13:29:21 -0700

    updating merge

--- client/src/pages/SearchFood.jsx
+++ client/src/pages/SearchFood.jsx
@@ -1,147 +1,224 @@
-// import React, { useState, useEffect } from "react";
-// import {
-//   Jumbotron,
-//   Container,
-//   Col,
-//   Form,
-//   Button,
-//   Card,
-//   CardColumns,
-// } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import {
+  Jumbotron,
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  CardColumns,
+} from "react-bootstrap";
 
-// import { useMutation } from "@apollo/client";
-// import { SAVE_FOOD } from "../utils/mutations";
-// import Auth from "../utils/auth";
-// import {searchFood} from "../utils/API";
-// import { saveFoodIds, getSavedFoodIds} from "../utils/localStorage";
+// import REACT_APP_API_KEY from ""
 
-// const SearchFood = () => {
-//   const [searchedFood, setSearchedFood] = useState([]);
-//   const [searchInput, setSearchInput] = useState("");
-//   const [savedFoodIds, setSavedFoodIds] = useState(getSavedFoodIds());
-//   const [foodSave] = useMutation(SAVE_FOOD);
+import { useMutation } from "@apollo/client";
+import { SAVE_FOOD } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { searchFoodApi } from "../utils/API";
+import { saveFoodIds, getSavedFoodIds } from "../utils/localStorage";
 
-//   useEffect(() => {
-//     return () => saveFoodIds(getSavedFoodIds);
-//   });
+// const SerpApi = require("google-search-results-nodejs");
+// const search = new SerpApi.GoogleSearch(
+//   "6aae3c12ac058815e5412d4c558836836b68960c22652694ca1320e7b5d10d83"
+// );
+const SearchFood = () => {
+  const [searchedFood, setSearchedFood] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [savedFoodIds, setSavedFoodIds] = useState(getSavedFoodIds());
+  const [foodSaved] = useMutation(SAVE_FOOD);
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
+  useEffect(() => {
+    return () => saveFoodIds(getSavedFoodIds);
+  });
 
-//     if (!searchInput) {
-//       return false;
-//     }
-//     try {
-//       const response = await searchFood(searchInput);
-//       if (!response.ok) {
-//         throw new Error("Oops! You got an error.");
-//       }
-//       const { items } = await response.json();
+  // const search = new SerpApi.GoogleSearch("6aae3c12ac058815e5412d4c558836836b68960c22652694ca1320e7b5d10d83");
+  // const result = search.json({
+  //   q: "Coffee",
+  //   location: "United States"
+  //  }, (result) => {
+  //    return result;
+  //  });
+  //  console.log(result)
+  //   fetch(
+  //     "https://serpapi.com/search.json?q=coffee&hl=en&gl=us&api_key=6aae3c12ac058815e5412d4c558836836b68960c22652694ca1320e7b5d10d83",
+  //     { mode: "no-cors" }
+  //   ).then(function (response) {
+  //     response.json().then(function (data) {
+  //       console.log(data);
+  //     });
+  //   });
+  const handleSubmit = async (e) => {
+    console.log("click");
+    e.preventDefault();
 
-//       const foodData = items.map((food) => ({
-//         // foodId: lorem,
-//         // recipe: lorem,
-//         // userId:lorem,
-//         // description: lorem,
-//         // image: lorem  will place our response data here
-//       }));
+    if (!searchInput) {
+      return false;
+    }
 
-//       setSearchedFood(foodData);
-//       setSearchInput("");
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
+    //     // if (!searchInput) {
+    //     //   return false;
+    //     // }
+    try {
+      const response = await searchFoodApi(searchInput);
 
-//   const handleSaveFood = async (foodId) => {
-//     const foodToSave = searchedFood.find((food) => food.foodId === foodId);
+      const { meals } = response;
+      console.log(meals);
+      const foodData = meals.map((meals) => ({
+        idMeal: meals.idMeal,
+        strMealThumb: meals.strMealThumb,
+        strMeal: meals.strMeal,
+        strIngredient1: meals.strIngredient1,
+        strIngredient2: meals.strIngredient2,
+        strIngredient3: meals.strIngredient3,
+        strInstructions: meals.strInstructions,
+        // strYoutube: meals.strYoutube
+      }));
+      // const foodData =meals.map((meals) => ({
 
-//     const token = Auth.loggedIn() ?  Auth.getToken() : null;
+      // }))
+      //         // const response = await fetch ()
+      //     // const response  = await fetch("https://cors-anywhere.serpapi.com/search.json?q=glutenfreebread&hl=en&gl=us&api_key=6aae3c12ac058815e5412d4c558836836b68960c22652694ca1320e7b5d10d83")
 
-//     if (!token) {
-//       return false;
-//     }
-//     try {
-//       await foodSave({
-//         variables: {content: foodToSave},
-//       });
-//       setSavedFoodIds([...savedFoodIds, foodToSave.foodId]);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
+      //     ), async (req, res) => {
+      //         // console.log(req.params.q)
+      //         //   const response = await axios.get(
+      //         //     `https://serpapi.com/search.json?q=${req.params.searchInput}&hl=en&gl=us&api_key=6aae3c12ac058815e5412d4c558836836b68960c22652694ca1320e7b5d10d83`
+      //         //   );
+      //         //   // console.log(response);
+      //         //   const json = await response.json
+      //         //   console.log(json)
+      //         //   res.json(response.data.recipes_results );
 
-//   // return (
-//   //   <>
-//   //     <Jumbotron fluid className="text-light bg-dark">
-//   //       <Container>
-//   //         <h1>Search for Books!</h1>
-//   //         <Form onSubmit={handleFormSubmit}>
-//   //           <Form.Row>
-//   //             <Col xs={12} md={8}>
-//   //               <Form.Control
-//   //                 name="searchInput"
-//   //                 value={searchInput}
-//   //                 onChange={(e) => setSearchInput(e.target.value)}
-//   //                 type="text"
-//   //                 size="lg"
-//   //                 placeholder="Search for a book"
-//   //               />
-//   //             </Col>
-//   //             <Col xs={12} md={4}>
-//   //               <Button type="submit" variant="success" size="lg">
-//   //                 Submit Search
-//   //               </Button>
-//   //             </Col>
-//   //           </Form.Row>
-//   //         </Form>
-//   //       </Container>
-//   //     </Jumbotron>
+      //         // const results = await searchFood(req.params.q, res);
+      //         // console.log(results);
+      //         // res.json(results);
+      //       };
+      // const response = await fetch(`/api/:q/${searchInput}`);
 
-//   //     <Container>
-//   //       <h2>
-//   //         {searchedBooks.length
-//   //           ? `Viewing ${searchedBooks.length} results:`
-//   //           : "Search for a book to begin"}
-//   //       </h2>
-//   //       <CardColumns>
-//   //         {searchedBooks.map((book) => {
-//   //           return (
-//   //             <Card key={book.bookId} border="dark">
-//   //               {book.image ? (
-//   //                 <Card.Img
-//   //                   src={book.image}
-//   //                   alt={`The cover for ${book.title}`}
-//   //                   variant="top"
-//   //                 />
-//   //               ) : null}
-//   //               <Card.Body>
-//   //                 <Card.Title>{book.title}</Card.Title>
-//   //                 <p className="small">Authors: {book.authors}</p>
-//   //                 <Card.Text>{book.description}</Card.Text>
-//   //                 {Auth.loggedIn() && (
-//   //                   <Button
-//   //                     disabled={savedBookIds?.some(
-//   //                       (savedBookId) => savedBookId === book.bookId
-//   //                     )}
-//   //                     className="btn-block btn-info"
-//   //                     onClick={() => handleSaveBook(book.bookId)}
-//   //                   >
-//   //                     {savedBookIds?.some(
-//   //                       (savedBookId) => savedBookId === book.bookId
-//   //                     )
-//   //                       ? "This book has already been saved!"
-//   //                       : "Save this Book!"}
-//   //                   </Button>
-//   //                 )}
-//   //               </Card.Body>
-//   //             </Card>
-//   //           );
-//   //         })}
-//   //       </CardColumns>
-//   //     </Container>
-//   //   </>
-//   // ); Will be replaced with our Material UI code
-// };
+      // if (!response.ok) {
+      //   throw new Error("Oops! You got an error.");
+      // }
+      // const { items } = await response.json();
+      // const body = await response.body;
+      // // const reader = body.getReader()
+      // const foodData = items.map((food) => ({
+      //   idMeal: idMeal,
+      //   title: food.title,
+      //   link: food.link,
+      //   source: food.source,
+      //   totalTime: food.totalTime,
+      //   ingredients: food.ingredients,
+      // }));
 
-// export default SearchFood;
+      setSearchedFood(foodData);
+      // console.log(reader.read())
+      setSearchInput("");
+    } catch (err) {
+      console.error("line 59", err);
+    }
+  };
+
+  const handleSaveFood = async (idMeal) => {
+    const foodToSave = searchedFood.find((food) => food.idMeal === idMeal);
+
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+    try {
+      await foodSaved({
+        variables: { content: foodToSave },
+      });
+      setSavedFoodIds([...savedFoodIds, foodToSave.idMeal]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <Jumbotron fluid className="text-light bg-dark">
+        <Container>
+          <h1>Search for Recipes!</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Row>
+              <Col xs={12} md={8}>
+                <Form.Control
+                  name="searchInput"
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  type="text"
+                  size="lg"
+                  placeholder="Search for a recipe"
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <Button type="submit" variant="success" size="lg">
+                  Submit Search
+                </Button>
+                {/* <Button onClick={handleSubmit} variant="success" size="lg">
+                  Test!
+                </Button> */}
+              </Col>
+            </Form.Row>
+          </Form>
+        </Container>
+      </Jumbotron>
+
+      <Container>
+        <h2>
+          {searchedFood.length
+            ? `Viewing ${searchedFood.length} results:`
+            : "Search for a recipe to begin"}
+        </h2>
+        <CardColumns>
+          {searchedFood.map((food) => {
+            return (
+              <Card key={food.idMeal} border="dark">
+                {food.strMealThumb ? (
+                  <Card.Img
+                    src={food.strMealThumb}
+                    alt={`The link to ${food.strMeal}`}
+                    variant="top"
+                  />
+                ) : null}
+                <Card.Body>
+                  <Card.Title>{food.strMeal}</Card.Title>
+                  <p className="small">
+                    Ingredients:{" "}
+                    {[
+                      food.strIngredient1,
+                      food.strIngredient2,
+                      food.strIngredient3,
+                    ]}
+                  </p>
+                  {/* <p className="small">Ingredients: {[food.strIngredient1.join(' '), food.strIngredient2.join(' '), food.strIngredient3]}</p> */}
+                  <Card.Text>{food.strInstructions}</Card.Text>
+                  {Auth.loggedIn() && (
+                    <Button
+                      disabled={savedFoodIds?.some(
+                        (savedFoodId) => savedFoodId === food.idMeal
+                      )}
+                      className="btn-block btn-info"
+                      onClick={() => handleSaveFood(food.idMeal)}
+                    >
+                      {savedFoodIds?.some(
+                        (savedFoodId) => savedFoodId === food.idMeal
+                      )
+                        ? "This recipe has already been saved!"
+                        : "Save this Recipe!"}
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+    //   ); Will be replaced with our Material UI code
+  );
+};
+
+export default SearchFood;
