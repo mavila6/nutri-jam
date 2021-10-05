e9222a18b5ed719a2e2f4bb69f711d769f47b464
Samantha Malone <sammac99@gmail.com>
2021-10-04 23:22:36 -0700

    added search box and dropdowns to front-end

---
+++ client/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,61 @@
+import React, { useState, Fragment } from "react";
+import SearchBox from "../SearchBox/SearchBox";
+import SearchBtn from "../Buttons/SearchBtn";
+import Dropdowns from "../Dropdowns/Dropdowns";
+
+function SearchBar(props) {
+  const [search, setSearch] = useState(null);
+  const [searchActive, setSearchActive] = useState(null);
+  const [filters, setFilters] = useState({
+    category: null,
+    area: null,
+  });
+  const [isPressed, setIsPressed] = useState(false);
+
+  const getSearchValue = (val) => {
+    setSearch(val);
+    setSearchActive(val);
+
+    setFilters({
+      category: null,
+      area: null,
+    });
+    setIsPressed(false);
+  };
+
+  const getFilterValue = (cat, area) => {
+    setFilters({
+      category: cat,
+      area: area,
+    });
+    setSearch(null);
+    setSearchActive(null);
+    setIsPressed(false);
+  };
+
+  const formPressedSubmit = () => {
+    setIsPressed(true);
+  };
+
+  return (
+    <Fragment>
+      <div className="flex flex-row w-full md:px-16 sm:flex-col justify-center mt-16 mb-8">
+        <SearchBox
+          search={getSearchValue}
+          isPressed={formPressedSubmit}
+          active={searchActive}
+        />
+        <SearchBtn
+          isPressed={isPressed}
+          searchVal={search}
+          filterVal={filters}
+        />
+      </div>
+      <div>
+        <Dropdowns filters={getFilterValue} active={filters} />
+      </div>
+    </Fragment>
+  );
+}
+
+export default SearchBar;
